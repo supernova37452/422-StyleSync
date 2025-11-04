611d149e1899aefa7242a6a3b5f32f6013d5017c
syhussein123 <147541072+syhussein123@users.noreply.github.com>
2025-11-04 16:56:32 -0600

    fixing some small issues

--- src/App.vue
+++ src/App.vue
@@ -1,57 +1,10 @@
-<script setup>
-import { ref } from "vue";
-
-const username = ref("Layla"); //this are reactive variables, we can inject anything here but obvi must write the logic 4 that
-const weather = ref("62");
-</script>
-<!-- okay so here is our app.vue: this is the top parent component! 
-from my understanding, each component in vue would be a screen, this is the closet view for now
--->
+<!-- App.vue -->
 <template>
   <router-view />
-  <!-- this is where the router will inject the components based on the route -->
-  <div class="header-bar">
-    <button class="button">{{ username }}</button>
-    <h1>Welcome to StyleSync</h1>
-    <button class="button">{{ weather }}</button>
-  </div>
-  <div class="all-groups">
-    <div class="one-group">
-      <h3>Tops</h3>
-      <div class="box-grid">
-        <div v-for="n in 8" :key="`tops-${n}`" class="box"></div>
-      </div>
-    </div>
-
-    <div class="one-group">
-      <h3>Bottoms</h3>
-      <div class="box-grid">
-        <div v-for="n in 8" :key="`bottoms-${n}`" class="box"></div>
-      </div>
-    </div>
-
-    <div class="one-group">
-      <h3>Shoes</h3>
-      <div class="box-grid">
-        <div v-for="n in 8" :key="`shoes-${n}`" class="box"></div>
-      </div>
-    </div>
-
-    <div class="one-group">
-      <h3>Jackets</h3>
-      <div class="box-grid">
-        <div v-for="n in 8" :key="`jackets-${n}`" class="box"></div>
-      </div>
-    </div>
-
-    <div class="one-group">
-      <h3>Accessories</h3>
-      <div class="box-grid">
-        <div v-for="n in 8" :key="`acc-${n}`" class="box"></div>
-      </div>
-    </div>
-  </div>
-  <p></p>
 </template>
+
+<script setup>
+// no page-specific logic here
+</script>
 
 <style scoped></style>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "./Login.vue";
-import App from "./App.vue";
+import Closet from "./Closet.vue";
 
 //all route definitions go here for everything
 const routes = [
   { path: "/", component: Login },
-  { path: "/closet", component: App },
+  { path: "/closet", component: Closet },
 ];
 
 const router = createRouter({
